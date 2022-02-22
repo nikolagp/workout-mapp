05ae7bb175ae6ea891238faa564535960a8df561
nikolagp <petrovskinikolag@gmail.com>
2022-02-22 18:37:01 +0100

    app architecture created

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Workout Map App created as a project of the JavaScript course of Jonas S.
 ## About the project
 
 The project consist a log of workouts using map api and log them in the browser.
+
+## In this project we use:
+
+- Geolocation
+- External Library Leaflet - an open-source JavaScript library
+  for mobile-friendly interactive maps
--- script.js
+++ script.js
@@ -11,12 +11,98 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log(position);
-    },
-    function () {
-      alert(`Sorry... We couldn't obtain your current position.`);
-    }
-  );
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration)
+}
+
+class Running extends Workout {
+
+}
+
+
+//////////////////////////////////
+// App Architecture 
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+    this._toggleElevationField();
+
+    form.addEventListener("submit", this._newWorkout.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`Sorry... We couldn't obtain your current position.`);
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling clicks on map
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    // Handling event for Cyclng and Running
+    inputType.addEventListener("change", function () {
+      inputElevation
+        .closest(".form__row")
+        .classList.toggle("form__row--hidden");
+      inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    });
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear Input Fields
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        "";
+
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("New Workout!")
+      .openPopup();
+  }
+}
+
+const app = new App();
